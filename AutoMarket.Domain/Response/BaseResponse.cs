51194f9b3a2de5f3c233ec7d149d4007a920ca56
pinkxpink00 <kyperu07@gmail.com>
2023-12-23 22:36:37 +0200

    create enum StatusCode, BaseResponse

---
+++ AutoMarket.Domain/Response/BaseResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMarket.Domain.Enum;
+
+namespace AutoMarket.Domain.Response
+{
+    public class BaseResponse<T> : IBaseResponse<T>
+    {
+        public string Description { get; set; }
+        
+        public StatusCode StatusCode { get; set; }
+
+        public T Data { get; }
+    }
+
+    public interface IBaseResponse<T>
+    {
+        T Data { get; }
+    }
+}
--- AutoMarket/Controllers/CarController.cs
+++ AutoMarket/Controllers/CarController.cs
@@ -6,32 +6,10 @@ namespace AutoMarket.Controllers
 {
     public class CarController : Controller
     {
-        private readonly ICarRepository _carRepository;
-
-        public CarController(ICarRepository carRepository)
-        {
-            _carRepository = carRepository;
-        }
-
+        
         [HttpGet]
         public async Task<IActionResult> GetCars()
         {
-            var response = await _carRepository.Select();
-            var secondResponse = await _carRepository.GetByName("CarName1");
-            var thirtyResponse = await _carRepository.GetAsync(3);
-
-            var newCar = new Car
-            {
-                Name = "BMW",
-                Model = "320",
-                Description = "Gasoline Auto",
-                Speed = 320,
-                Price = 1,
-                DateCreate = DateTime.UtcNow
-            };
-            
-            await _carRepository.Create(newCar);
-            await _carRepository.Delete(newCar);
             return View(response);
         }
     }
