191933a8be9b06fbb51e0e2ea81c21821045ea03
pinkxpink00 <kyperu07@gmail.com>
2023-12-29 22:43:36 +0200

    create IQuareble method GetAll()

--- AutoMarket.DAL/Interfaces/IBaseRepository.cs
+++ AutoMarket.DAL/Interfaces/IBaseRepository.cs
@@ -3,9 +3,8 @@
     public interface IBaseRepository<T>
     {
         Task<bool> Create(T entity);
-        Task <T> Get(int id);
-
-        Task<List<T>> Select();
+        
+        IQueryable<T> GetAll ();
 
         Task<bool> Delete(T entity);
 
--- AutoMarket/Program.cs
+++ AutoMarket/Program.cs
@@ -36,6 +36,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
